e3faad3c7f0f9cdae2ed0f7116be7e7334a7658e
EthanPaleyron <ethan.paleyron@gmail.com>
2025-06-19 17:00:44 +0200

    Add article management features: create, edit, delete, and show articles

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -4,8 +4,12 @@ namespace App\Controller;
 
 use App\Entity\Article;
 use App\Repository\ArticleRepository;
+use App\Form\ArticleType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ArticleController extends AbstractController
@@ -18,5 +22,54 @@ class ArticleController extends AbstractController
         return $this->render('article/index.html.twig', [
             'articles' => $articles,
         ]);
+    }
+
+    #[Route('/articles/new', name: 'article_new')]
+    public function new(Request $request, EntityManagerInterface $em): Response
+    {
+        $article = new Article();
+        $form = $this->createForm(ArticleType::class, $article);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($article);
+            $em->flush();
+            return $this->redirectToRoute('article_list');
+        }
+        return $this->render('article/new.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/articles/{id}', name: 'article_show', requirements: ['id' => '\\d+'])]
+    public function show(Article $article): Response
+    {
+        return $this->render('article/show.html.twig', [
+            'article' => $article,
+        ]);
+    }
+
+    #[Route('/articles/{id}/edit', name: 'article_edit', requirements: ['id' => '\\d+'])]
+    public function edit(Request $request, Article $article, EntityManagerInterface $em): Response
+    {
+        $form = $this->createForm(ArticleType::class, $article);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->flush();
+            return $this->redirectToRoute('article_list');
+        }
+        return $this->render('article/edit.html.twig', [
+            'form' => $form->createView(),
+            'article' => $article,
+        ]);
+    }
+
+    #[Route('/articles/{id}/delete', name: 'article_delete', methods: ['POST'], requirements: ['id' => '\\d+'])]
+    public function delete(Request $request, Article $article, EntityManagerInterface $em): RedirectResponse
+    {
+        if ($this->isCsrfTokenValid('delete' . $article->getId(), $request->request->get('_token'))) {
+            $em->remove($article);
+            $em->flush();
+        }
+        return $this->redirectToRoute('article_list');
     }
 }
